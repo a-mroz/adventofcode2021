0e8543e1a2cff0c7355e34b46becd08e5839473a
Adam Mróz <adam.michal.mroz@gmail.com>
2021-12-15 12:08:06 +0100

    Day 15 - part 2

--- 15.py
+++ 15.py
@@ -15,24 +15,7 @@ dr = [-1, 0, 1, 0]
 dc = [0, 1, 0, -1]
 
 
-# ((r, c,), dist)
-graph = {}
-for r in range(R):
-    for c in range(C):
-        links = []
 
-        for i in range(4):
-            rr = dr[i] + r
-            cc = dc[i] + c
-
-            if 0 <= rr < R and 0 <= cc < C:
-                links.append(((rr, cc), G[rr][cc]))
-
-            graph[(r, c)] = links
-
-
-
-print(graph)
 
 def dijkstra(G, start, end):
    seen = set()
@@ -57,4 +40,68 @@ def dijkstra(G, start, end):
 
 
 
+# part 1
+graph = {}
+for r in range(R):
+    for c in range(C):
+        links = []
+
+        for i in range(4):
+            rr = dr[i] + r
+            cc = dc[i] + c
+
+            if 0 <= rr < R and 0 <= cc < C:
+                links.append(((rr, cc), G[rr][cc]))
+
+            graph[(r, c)] = links
+
+print(dijkstra(graph, (0, 0), (R - 1, C - 1)))
+
+
+# expand horizontally
+for r in G:
+    for i in range(1, 5):
+        for c in range(C):
+            new_c_idx = c + i * C
+            previous_c_idx = new_c_idx - C
+
+            r.append((r[previous_c_idx] + 1) % 10 or 1)
+
+# print('g2', len(G), len(G[0]))
+# print(G)
+
+
+# expand vertically
+for i in range(1, 5):
+    for r in range(R):
+        new_r_idx = r + i * R
+        previous_r_idx = new_r_idx = new_r_idx - R
+
+        G.append([(x + 1) % 10 or 1 for x in G[previous_r_idx]])
+
+R = len(G)
+C = len(G[0])
+print(R, C)
+# print(G[9][4])
+# print(G[19][4])
+# print(G[29][4])
+# print(G[39][4])
+# print(G[49][4])
+
+
+graph = {}
+for r in range(R):
+    for c in range(C):
+        links = []
+
+        for i in range(4):
+            rr = dr[i] + r
+            cc = dc[i] + c
+
+            if 0 <= rr < R and 0 <= cc < C:
+                links.append(((rr, cc), G[rr][cc]))
+
+            graph[(r, c)] = links
+
+# print(graph)
 print(dijkstra(graph, (0, 0), (R - 1, C - 1)))
